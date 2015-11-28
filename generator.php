1af0bd9a299ae18d63a4cd66080cb03fb1ad3926
akhfa <akhmadfakhoni@gmail.com>
2015-11-28 21:07:17 +0700

    get hostname done

--- generator.php
+++ generator.php
@@ -1,6 +1,7 @@
 <?php
 // referensi
 // http://stackoverflow.com/questions/1415577/accessing-variables-and-methods-outside-of-class-definitions
+	$nama_file;
 	$hostname;
 	$password;
 	$databaseName;
@@ -18,7 +19,7 @@
 
 	$tables = array(); //-> untuk ngisi array, pakai array put
 
-	Class FileManager{
+	Class JsonFileManager{
 		// Melakukan pengecekan pada konfigurasi file, apakah ada kesalahan
 		var $nama_file;
 		public function cekKonfigurasi(){
@@ -28,6 +29,44 @@
 		// Masukkan semua objek di file konfigurasi ke variable global. Baca referensi.
 		public function importToVariable(){
 
+		}
+	}
+
+	Class TextFileManager{
+		private $nama_file;
+
+		function setNamaFile($_filename)
+		{
+			$this->nama_file = $_filename;
+		}
+
+		function getNamaFile()
+		{
+			return $this->nama_file;
+		}
+
+		function cekKonfigurasi(){
+
+		}
+
+		// Masukkan semua objek di file konfigurasi ke variable global. Baca referensi.
+		function importToVariable(){
+			global $hostname;
+			global $password;
+			global $databaseName;
+			global $tables;
+
+			$myfile = fopen($this->nama_file, "r") or die("Unable to open file!");
+			// Output one line until end-of-file
+			while(!feof($myfile)) {
+			  	$line = fgets($myfile);
+				if(strpos($line, "ostname") === 1)
+				{
+					$line = explode(" ", $line);
+					$hostname = $line[1];
+				}
+			}
+			fclose($myfile);
 		}
 	}
 
@@ -41,7 +80,7 @@
 
 	Class ModelManager{
 		// Bikin file model dari yii2 nya di sini. Ambil data dari variable
-		public function makeModel{
+		public function makeModel(){
 			$file_model = fopen("newfile.php", "w") or die("Unable to open file!");
 			$template = "<?php
 
@@ -100,6 +139,22 @@ class Negara extends \yii\db\ActiveRecord
 	}
 	
 	// contoh jalananin fungsi dalam sebuah kelas
-	$model = new ModelManager;
-	$model->makeModel();
+	// $model = new ModelManager;
+	// $model->makeModel();
+
+	// Mulai dari sini udah beneran yaaa
+	//Read argument
+	$nama_file = $argv[1];
+
+	if(is_null($nama_file))
+		echo "Please set file name as an argument\n";
+	else
+	{
+		$readFile = new TextFileManager;
+		$readFile->setNamaFile($nama_file);
+		$readFile->importToVariable();
+		echo $hostname;
+//		echo $nama_file."\n";
+//		echo $readFile->nama_file;
+	}
 ?>
